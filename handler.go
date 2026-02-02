aa19743006541af771bb34563ab8884e90565e02
Kirill Belyaev <kirill10121415@gmail.com>
2026-02-02 16:18:00 +0300

    feat: separate handler and main add fallback situation

---
+++ handler.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Начало запроса")
+	resp, err := http.Get("https://dog.ceo/api/breeds/image/random")
+	if err != nil {
+		serverFallback(w)
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var respStruct randomResp
+	if err = json.Unmarshal(body, &respStruct); err != nil {
+		serverFallback(w)
+		return
+	}
+
+	if respStruct.Status != "success" {
+		serverFallback(w)
+		return
+	}
+
+	imgResp, err := http.Get(respStruct.Message)
+	if err != nil {
+		serverFallback(w)
+	}
+	defer imgResp.Body.Close()
+	fmt.Println(imgResp.Header.Get("Content-Type"))
+	w.Header().Set("Content-Type", imgResp.Header.Get("Content-Type"))
+	w.WriteHeader(http.StatusOK)
+
+	_, err = io.Copy(w, imgResp.Body)
+	if err != nil {
+		serverFallback(w)
+	}
+	fmt.Println("Конец запроса")
+}
+
+func serverFallback(w http.ResponseWriter) {
+	f, _ := os.Open("dropPhoto.jpg")
+	defer f.Close()
+
+	w.Header().Set("Content-Type", "image/jpeg")
+	io.Copy(w, f)
+}
--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 )
 
@@ -14,42 +12,7 @@ type randomResp struct {
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-
-		fmt.Println("Начало запроса")
-		resp, err := http.Get("https://dog.ceo/api/breeds/image/random")
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		defer resp.Body.Close()
-		body, _ := io.ReadAll(resp.Body)
-		var respStruct randomResp
-		if err = json.Unmarshal(body, &respStruct); err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		if respStruct.Status != "success" {
-			http.Error(w, "dog.ceo api error", 500)
-			return
-		}
-
-		imgResp, err := http.Get(respStruct.Message)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-		}
-		defer imgResp.Body.Close()
-
-		w.Header().Set("Content-Type", imgResp.Header.Get("Content-Type"))
-		w.WriteHeader(http.StatusOK)
-
-		_, err = io.Copy(w, imgResp.Body)
-		if err != nil {
-			fmt.Printf("streaming error: %v", err)
-		}
-		fmt.Println("Конец запроса")
-	})
+	http.HandleFunc("/", MainHandler)
 
 	fmt.Println("server is started on :8080")
 
